6fd0cb4162afb185328113786dc51cafb9b17dd2
“kidanikouki” <kidanikouki@gmail.com>
2024-10-26 17:11:18 +0900

    functions-beforedeploy

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,21 @@
 const functions = require("firebase-functions");
+const express = require("express");
+const path = require("path");
 
-// Create and Deploy Your First Cloud Functions
-// https://firebase.google.com/docs/functions/write-firebase-functions
+const app = express();
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", { structuredData: true });
-  response.send("Hello fromaaaa Firebase!");
+// 静的ファイルの提供設定
+app.use(express.static(path.join(__dirname, "../public"))); // 正しいパスに修正
+
+// ルートにアクセスがあった場合に index.html を提供する
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
 });
+
+// 他のパスを `/` にリダイレクトする（任意）
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+// Firebase Functions でエクスポート
+exports.api = functions.https.onRequest(app);
