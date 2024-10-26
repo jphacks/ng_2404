5abe2affdcaa0d8c37c5864415bcf23bb7f9cde7
Fuu <fukimax0612@gmail.com>
2024-10-27 02:39:51 +0900

    rm posts.tsx

--- pop-turn/src/pages/Posts/Posts.tsx
+++
@@ -1,9 +0,0 @@
-import { Text } from "@chakra-ui/react";
-
-export default function Home() {
-  return (
-    <Text>
-      これは仮置きだからコンフリクト起きたら削除してね！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！
-    </Text>
-  );
-}
