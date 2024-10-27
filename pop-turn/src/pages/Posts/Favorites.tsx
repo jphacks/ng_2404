572b4e4e6a0ea0e56cc67fda2f6b9ad40ca7b2d0
Fuu <fukimax0612@gmail.com>
2024-10-27 12:26:25 +0900

    fix Favpage

--- pop-turn/src/pages/Posts/Favorites.tsx
+++ pop-turn/src/pages/Posts/Favorites.tsx
@@ -6,7 +6,7 @@ function Home() {
   return (
     <div className="h-screen bg-white flex items-center justify-content-space-between">
       <Sidebar currentPage="Favorites" />
-      <div className="w-[calc(100%-13rem)] ml-[13rem] h-full px-2 mt-10">
+      <div className="w-[calc(100%-13rem)] ml-[13rem] h-full px-20 mt-10">
       <PostPageTemplate type="Favorites" />
       </div>
     </div>
